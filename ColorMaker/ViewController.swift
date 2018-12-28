27c03718399f129b120ea4c0ea55ff20f263152c
Jasmeet Singh <jasmeetsinghbhatia@gmail.com>
2018-12-28 00:29:23 -0500

    Project- ColorMaker, completed, bug fixes in upcoming commits

--- ColorMaker/ViewController.swift
+++ ColorMaker/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var canvas: UIView!
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        canvas.backgroundColor = UIColor.black
     }
-
-
+    
+    @IBAction func changeCanvasColor(_ sender: Any) {
+        
+        // Make sure the program doesn't crash if the controls aren't connected
+        if self.redSwitch == nil {
+            return
+        }
+        
+        let r: CGFloat = self.redSwitch.isOn ? 1 : 0
+        let b: CGFloat = self.blueSwitch.isOn ? 1 : 0
+        let g: CGFloat = self.greenSwitch.isOn ? 1 : 0
+        
+        canvas.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
 }
 
